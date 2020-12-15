995692bbdf55f196fdf763e209e970bcd8b1e824
Frederico Cruz <frederico.cruz@datacruz.com.br>
2020-12-15 11:08:10 -0300

    adicionado midleware de seguranca.

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,17 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
 
 
 Route::resource('/usuarios', 'App\Http\Controllers\UsuariosController');
-Route::resource('/pacientes', 'App\Http\Controllers\PacientesController');
-Route::resource('/medicos', 'App\Http\Controllers\MedicosController');
-Route::resource('/agendamentos', 'App\Http\Controllers\AgendamentosController');
+Route::resource('/pacientes', 'App\Http\Controllers\PacientesController')->middleware(['auth:sanctum', 'verified']);
+Route::resource('/medicos', 'App\Http\Controllers\MedicosController')->middleware(['auth:sanctum', 'verified']);
+Route::resource('/agendamentos', 'App\Http\Controllers\AgendamentosController')->middleware(['auth:sanctum', 'verified']);
 
 
 
-Route::get("/getUsuarios", "App\Http\Controllers\UsuariosController@ajaxConsultarUsuarios")->name("ajax.usuarios.consultar");
-Route::get("/getPacientes", "App\Http\Controllers\PacientesController@ajaxConsultarPacientes")->name("ajax.pacientes.consultar");
-Route::get("/getMedicos", "App\Http\Controllers\MedicosController@ajaxConsultarMedicos")->name("ajax.medicos.consultar");
-Route::get("/getAgendamentos", "App\Http\Controllers\AgendamentosController@ajaxConsultarAgendamentos")->name("ajax.agendamentos.consultar");
-
-Route::get("/getMedicoAutoComplete", "App\Http\Controllers\AgendamentosController@ajaxConsultarMedicoAutoComplete")->name("ajax.medicoautocomplete.consultar");
+Route::get("/getUsuarios", "App\Http\Controllers\UsuariosController@ajaxConsultarUsuarios")->name("ajax.usuarios.consultar")->middleware(['auth:sanctum', 'verified']);
+Route::get("/getPacientes", "App\Http\Controllers\PacientesController@ajaxConsultarPacientes")->name("ajax.pacientes.consultar")->middleware(['auth:sanctum', 'verified']);
+Route::get("/getMedicos", "App\Http\Controllers\MedicosController@ajaxConsultarMedicos")->name("ajax.medicos.consultar")->middleware(['auth:sanctum', 'verified']);
+Route::get("/getAgendamentos", "App\Http\Controllers\AgendamentosController@ajaxConsultarAgendamentos")->name("ajax.agendamentos.consultar")->middleware(['auth:sanctum', 'verified']);
+Route::get("/getMedicoAutoComplete", "App\Http\Controllers\AgendamentosController@ajaxConsultarMedicoAutoComplete")->name("ajax.medicoautocomplete.consultar")->middleware(['auth:sanctum', 'verified']);
 
 
 
